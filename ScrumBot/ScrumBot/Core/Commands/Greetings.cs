e47e2d172b2c375affed67ac380d7b5de9c8682a
Doug <daferrib@mtu.edu>
2019-02-20 13:51:50 -0500

    Added ScrumBot main Created Greetings Script Added token to data Organized files a little Commit (2/20/2019)

---
+++ ScrumBot/ScrumBot/Core/Commands/Greetings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScrumBot.Core.Commands
+{
+    public class Greetings
+    {
+            
+    }
+}
---
+++ ScrumBot/ScrumBot/ScrumBot.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private DiscordSocketClient Client;
+        private CommandService Commands;
+        private IServiceProvider SP = null;
+
+
+        static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+            });
+
+            Commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = true,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+            });
+
+            Client.MessageReceived += Client_MessageRecieved;
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), SP);
+
+            Client.Ready += Client_Ready;
+            Client.Log += Client_Log;
+
+            string Token;
+            using (var Stream = new FileStream((Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).Replace(@"bin\Debug\netcoreapp2.1", @"Data\Token.txt"), FileMode.Open, FileAccess.Read))
+            using (var ReadToken = new StreamReader(Stream))
+            {
+                Token = ReadToken.ReadToEnd();
+            }
+            await Client.LoginAsync(TokenType.Bot, Token);
+            await Client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+
+        private async Task Client_MessageRecieved(SocketMessage messageParam)
+        {
+            var Message = messageParam as SocketUserMessage;
+            var Context = new SocketCommandContext(Client, Message);
+
+            if (Context.Message == null || Context.Message.Content == "") return;
+            if (Context.User.IsBot) return;
+
+            int ArgPos = 0;
+            if(!(Message.HasStringPrefix(".", ref ArgPos) || Message.HasMentionPrefix(Client.CurrentUser, ref ArgPos))) return;
+
+            var Result = await Commands.ExecuteAsync(Context, ArgPos, SP);
+            if (!Result.IsSuccess)
+            {
+                Console.WriteLine($"{DateTime.Now} at Commands] Something when wrong with executing command. Text: {Context.Message.Content} | Error: {Result.ErrorReason}");
+            }
+        }
+
+        //Sets the bots status in discord
+        private async Task Client_Ready()
+        {
+            await Client.SetGameAsync("Scrum Master Simulator");
+        }
+
+
+        private async Task Client_Log(LogMessage Message)
+        {
+            Console.WriteLine($"{DateTime.Now} at {Message.Source}] {Message.Message}");
+        }
+    }
+}
