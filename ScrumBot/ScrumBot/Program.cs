1a5e2d6e385a08e650f62662263ec1d47d3d0984
Doug <daferrib@mtu.edu>
2019-02-20 13:02:49 -0500

    Created Visual Studio Integration

---
+++ ScrumBot/ScrumBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScrumBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
