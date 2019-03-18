6963618cf16ec24483152e89f6b080fc4c6964c6
Austen Goddu <ajgoddu@gmail.com>
2019-03-18 13:51:23 -0400

    Re-added using statement for status enumeration in Commands.cs

--- ScrumBot/ScrumBot/Core/Commands/Commands.cs
+++ ScrumBot/ScrumBot/Core/Commands/Commands.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 
+using ScrumBot.Core.HelperData;
+
 namespace ScrumBot.Core.Commands
 {
     /**
@@ -13,93 +15,91 @@ namespace ScrumBot.Core.Commands
      */
     public class Job
     {
-        public class Job
+        private string title;
+        private string developer;
+        private string description;
+        private status jobStatus;
+        private DateTime dateCreated;
+        private DateTime dateLastModified;
+        private DateTime dateFinished;
+
+
+        public Job(string title, string developer, string description)
         {
-            private string title;
-            private string developer;
-            private string description;
-            private status jobStatus;
-            private DateTime dateCreated;
-            private DateTime dateLastModified;
-            private DateTime dateFinished;
-
-
-            public Job(string title, string developer, string description)
-            {
-                this.description = description;
-                this.developer = developer;
-                this.description = description;
-                this.jobStatus = status.NOT_STARTED;
-                dateCreated = DateTime.Now;
-            }
-
-            public Job() { }
-
-            public void SetTitle(string title)
-            {
-                this.title = title;
-            }
-
-            public string GetTitle()
-            {
-                return title;
-            }
-
-            public void SetDeveloper(string developer)
-            {
-                this.developer = developer;
-            }
-
-            public string GetDeveloper()
-            {
-                return developer;
-            }
-
-            public void SetDescription(string description)
-            {
-                this.description = description;
-            }
-
-            public string GetDescription()
-            {
-                return description;
-            }
-            public DateTime getDateCreated()
-            {
-                return dateCreated;
-            }
-            public status getStatus()
-            {
-                return jobStatus;
-            }
-            public DateTime getDateLastModified()
-            {
-                return dateLastModified;
-            }
-            public DateTime getDateFinished()
-            {
-                return dateFinished;
-            }
-
+            this.description = description;
+            this.developer = developer;
+            this.description = description;
+            this.jobStatus = status.NOT_STARTED;
+            dateCreated = DateTime.Now;
         }
 
-        public class Story : Job
+        public Job() { }
+
+        public void SetTitle(string title)
         {
-            private int sprintNum;
-            private DateTime sprintEndDate;
-
-            public Story(string title, string developer, string description, int sprint) : base(title, developer, description)
-            {
-                sprintNum = sprint;
-            }
-
-            public Story() { }
+            this.title = title;
         }
 
-        /**
-         * Commands class contains all of the commands for the bot.
-         */
-        public class Commands : ModuleBase<SocketCommandContext>
+        public string GetTitle()
+        {
+            return title;
+        }
+
+        public void SetDeveloper(string developer)
+        {
+            this.developer = developer;
+        }
+
+        public string GetDeveloper()
+        {
+            return developer;
+        }
+
+        public void SetDescription(string description)
+        {
+            this.description = description;
+        }
+
+        public string GetDescription()
+        {
+            return description;
+        }
+        public DateTime getDateCreated()
+        {
+            return dateCreated;
+        }
+        public status getStatus()
+        {
+            return jobStatus;
+        }
+        public DateTime getDateLastModified()
+        {
+            return dateLastModified;
+        }
+        public DateTime getDateFinished()
+        {
+            return dateFinished;
+        }
+
+    }
+
+    public class Story : Job
+    {
+        private int sprintNum;
+        private DateTime sprintEndDate;
+
+        public Story(string title, string developer, string description, int sprint) : base(title, developer, description)
+        {
+            sprintNum = sprint;
+        }
+
+        public Story() { }
+    }
+
+    /**
+     * Commands class contains all of the commands for the bot.
+     */
+    public class Commands : ModuleBase<SocketCommandContext>
     {
         List<Job> jobs = new List<Job>(); // A collection for all the jobs to add in a session.
 
