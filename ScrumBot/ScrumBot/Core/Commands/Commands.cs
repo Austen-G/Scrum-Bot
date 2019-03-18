ea40232575507a11072083c2d536c99e36eb6ec8
bdreece <32660954+bdreece@users.noreply.github.com>
2019-03-18 13:26:46 -0400

    Updated Commands.cs
    Uses a '~' as a delimiter when reading in task information.

--- ScrumBot/ScrumBot/Core/Commands/Commands.cs
+++ ScrumBot/ScrumBot/Core/Commands/Commands.cs
@@ -8,6 +8,9 @@ using Discord.Commands;
 
 namespace ScrumBot.Core.Commands
 {
+    /**
+     * Job class is an object containing basic data about a particular job
+     */
     public class Job
     {
         private string title;
@@ -54,16 +57,37 @@ namespace ScrumBot.Core.Commands
         }
     }
 
+    /**
+     * Commands class contains all of the commands for the bot.
+     */
     public class Commands : ModuleBase<SocketCommandContext>
     {
-        List<Job> tasks = new List<Job>();
+        List<Job> jobs = new List<Job>(); // A collection for all the jobs to add in a session.
 
+        /**
+         *  Defines a task based on given parameters, and adds it to jobs.
+         *      [Remainder]string param - Entire substring after the command and space.
+         */
         [Command("CreateTask"), Alias("createTask"), Summary("Allows the user to create a task for their project")]
-        public async Task createTask(string title, string developer, string description )
+        public async Task CreateTask([Remainder]string param)
         {
-            tasks.Add(new Job(title, developer, description));
-
-            await Context.Channel.SendMessageAsync("Created task titled: '" + title + "' for '" + developer + "' with the following description: '" + description + "'.");
+            string[] args = param.Split('~'); // Parses parameters using '~' delimiter.
+            // Provides the syntax to create a task
+            if (args.Length < 3)
+            {
+                await ReplyAsync("CreateTask <task>-<developer>-<description>");
+                return;
+            }
+            else
+            {
+                jobs.Add(new Job(args[0], args[1], args[2])); // Adds it to jobs
+                // Mentions user in reply if user is not a bot
+                if (!Context.User.IsBot)
+                    await ReplyAsync(Context.User.Mention + " Task: '" + args[0] + "', Developer: '" + args[1] + "', Description: '" + args[2] + "'.");
+                else
+                    await ReplyAsync("Task: '" + args[0] + "', Developer: '" + args[1] + "', Description: '" + args[2] + "'.");
+                // Replies to the user, confirming the task.
+            }
         }
     }
 }
