07b5968c2745ec1637f0e7c53bdc31ab28d9565c
Doug <daferrib@mtu.edu>
2019-03-20 13:47:25 -0400

    Added Help Function
    Created embed c# file and text file listing commands

--- ScrumBot/ScrumBot/Core/Commands/Greetings.cs
+++ ScrumBot/ScrumBot/Core/Commands/Greetings.cs
@@ -26,4 +26,29 @@ namespace ScrumBot.Core.Commands
         [Command("Echo"), Alias("echo")]
         public async Task Echo([Remainder]string text) => await ReplyAsync(text);
     }
+
+    /**
+         *  Sends a list of commands that can be used
+         */
+    [Command("Help")]
+    public async Task Echo()
+    {
+        // Get reminders
+        try
+        {
+            using (StreamReader sr = File.OpenText("Help.txt"))
+            {
+                string text;
+                while ((text = sr.ReadLine()) != null)
+                {
+                    // Send response
+                    await Context.Channel.SendMessageAsync(text);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
 }
---
+++ ScrumBot/ScrumBot/Core/HelperData/WriteToEmbed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+
+using Discord;
+using Discord.Commands;
+
+namespace ScrumBot.Core.HelperData
+{
+    class WriteToEmbed
+    {
+        //create an embedded message
+        //@todo
+        public createEmbed()
+        {
+            EmbedBuilder embed = new EmbedBuilder();
+        }
+    }
+}
