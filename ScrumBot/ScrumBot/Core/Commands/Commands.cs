bbfc0bf63ce851e2d179a10f1153fa8467bc5714
brianreece889 <brianreece889@gmail.com>
2019-03-29 13:25:51 -0400

    Changed name of CreateTask to CreateJob for continuity's sake

--- ScrumBot/ScrumBot/Core/Commands/Commands.cs
+++ ScrumBot/ScrumBot/Core/Commands/Commands.cs
@@ -23,8 +23,8 @@ namespace ScrumBot.Core.Commands
          *  Defines a task based on given parameters, and adds it to jobs.
          *      [Remainder]string param - Entire substring after the command and space.
          */
-        [Command("CreateTask"), Alias("createTask"), Summary("Allows the user to create a task for their project")]
-        public async Task CreateTask([Remainder]string param)
+        [Command("CreateJob"), Alias("createJob"), Summary("Allows the user to create a job for their project")]
+        public async Task CreateJob([Remainder]string param)
         {
             string[] args = param.Split('~'); // Parses parameters using '~' delimiter.
             // Provides the syntax to create a task
@@ -33,28 +33,29 @@ namespace ScrumBot.Core.Commands
                 var eb = new EmbedBuilder();
 
                 eb.WithColor(Color.Orange);
-                eb.WithTitle("CreateTask");
+                eb.WithTitle("CreateJob");
                 eb.WithAuthor("ScrumBot");
-                eb.WithDescription("Creates a task for a given user, with the following criteria:");
-                eb.AddField(".CreateTask", "Command name");
+                eb.WithDescription("Creates a job for a given user, with the following criteria:");
+                eb.AddField(".CreateJob", "Command name");
                 eb.AddField("<title>", "Title of the task");
                 eb.AddField("<developer>", "Name of developer assigned to task");
                 eb.AddField("<description>", "Description of the task");
-                eb.AddField("[jobStatus]", "(0) NOT_STARTED, (1) IN_PROGRESS, (2) NEEDS_TESTING, (3) IN_TESTING, (4) DONE, (5) COMPLETE");
                 eb.WithFooter("Thank you!");
 
                 await Context.Channel.SendMessageAsync("", false, eb.Build());
-            } else {  
+            } else {
+                Job job = new Job(args[0], args[1], args[2]);
+                jobs.Add(job);
+
                 var eb = new EmbedBuilder();
 
                 eb.WithColor(Color.Orange);
-                eb.WithTitle("Task Created Successfully!");
+                eb.WithTitle("Job Created Successfully!");
                 eb.WithAuthor("ScrumBot");
-                eb.WithDescription("Task '" + args[0] + "' created and saved successfully.");
+                eb.WithDescription("Job '" + args[0] + "' created and saved successfully.");
                 eb.AddField("Title:", args[0]);
                 eb.AddField("Developer:", args[1]);
                 eb.AddField("Description:", args[2]);
-                eb.AddField("Job Status:", args[3]);
                 eb.WithFooter("Thank you!");
 
                 await Context.Channel.SendMessageAsync("", false, eb.Build());
