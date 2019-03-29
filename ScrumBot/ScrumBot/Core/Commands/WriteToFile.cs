31c2356a8ac997ee575fbe5dd1159531aac23e4a
brianreece889 <brianreece889@gmail.com>
2019-03-29 13:17:55 -0400

    Updated WriteToFile with embed formatting.

--- ScrumBot/ScrumBot/Core/Commands/WriteToFile.cs
+++ ScrumBot/ScrumBot/Core/Commands/WriteToFile.cs
@@ -14,31 +14,55 @@ namespace ScrumBot.Core.Commands
         ReadAndWrite rw = new ReadAndWrite();
 
         [Command("CreateFile")]
-        public async Task CreateFile(params String[] stringArray)
+        public async Task CreateFile([Remainder]String param)
         {
-            if (stringArray.Length != 3)
+            String[] args = param.Split("~");
+
+            var eb = new EmbedBuilder();
+
+            eb.WithColor(Color.Orange);
+            eb.WithAuthor("ScrumBot");
+            eb.WithTitle("CreateFile");
+            eb.WithDescription("Creates a file with the given textual information.");
+            eb.WithFooter("Thank you!");
+
+            if (args.Length < 2)
             {
-                await ReplyAsync("Incorrect number of parameters: .CreateFile filename text");
+                eb.AddField("Incorrect syntax!", ".CreateFile <filename>~<text>", true);
             }
             else
             {
-                rw.Write(stringArray[0], stringArray[1]);
-                await ReplyAsync("File Edited");
+                rw.Write(args[0], args[1]);
+                eb.AddField("File '" + args[0] + "' created successfully!", args[1], true);
             }
+
+            await Context.Channel.SendMessageAsync("", false, eb.Build());
         }
 
         [Command("Edit")]
-        public async Task EditFile(params String[] stringArray)
+        public async Task EditFile([Remainder]String param)
         {
-            if(stringArray.Length != 3)
+            String[] args = param.Split("~");
+
+            var eb = new EmbedBuilder();
+
+            eb.WithColor(Color.Orange);
+            eb.WithAuthor("ScrumBot");
+            eb.WithTitle("EditFile");
+            eb.WithDescription("Appends a file at a given section of text.");
+            eb.WithFooter("Thank you!");
+
+            if(args.Length < 3)
             {
-                await ReplyAsync("Incorrect number of parameters: .Edit filename section newText");
+                eb.AddField("Incorrect syntax!", ".EditFile <filename>~<section>~<newText>", true);
             }
             else
             {
-                rw.EditSection(stringArray[0], stringArray[1], stringArray[2]);
-                await ReplyAsync("File Edited");
+                rw.EditSection(args[0], args[1], args[2]);
+                eb.AddField("File '" + args[0] + "' edited successfully!", "'" + args[2] + "' added at section: " + args[1]);
             }
+
+            await Context.Channel.SendMessageAsync("", false, eb.Build());
         }
     }
 }
