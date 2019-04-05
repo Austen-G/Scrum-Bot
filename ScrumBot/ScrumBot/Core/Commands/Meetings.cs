7f0472b4339829a1976739b884e7041d22bfc649
Doug <daferrib@mtu.edu>
2019-04-05 01:36:34 -0400

    Create Meetings.cs
    Unstarted methods

---
+++ ScrumBot/ScrumBot/Core/Commands/Meetings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+
+using ScrumBot.Core;
+
+namespace ScrumBot.Core.Commands
+{
+    class Meetings : ModuleBase<SocketCommandContext>
+    {
+        /**
+         * sets up a list of topics for a meeting
+         */
+        [Command("MeetingHelp")]
+        public async Task Meeting([Remainder]String param)
+        {
+            if (param.Length != 3)
+            {
+                await ReplyAsync("Incorrect format: Name, ");
+            }
+            
+        }
+    }
+}
