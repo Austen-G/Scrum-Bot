69727b8078b95ba7a710fba927828e2b3099ca04
Austen Goddu <ajgoddu@gmail.com>
2019-04-10 13:39:01 -0400

    Added accessors and mutators in story class for task list, created different status lists for the scrum board object.

--- ScrumBot/ScrumBot/Core/ScrumBoard.cs
+++ ScrumBot/ScrumBot/Core/ScrumBoard.cs
@@ -6,6 +6,15 @@ namespace ScrumBot.Core
 {
     class ScrumBoard
     {
+        // different lists for story progress
+        List<Story> notStarted = new List<Story>();
+        List<Story> inProgress = new List<Story>();
+        List<Story> inTesting = new List<Story>();
+        List<Story> done = new List<Story>();
+        List<Story> finished = new List<Story>(); 
+
+        
+
 
     }
 }
